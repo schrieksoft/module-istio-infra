855d37867c45179b23d4599e49c135d6afc1f689
Karl Schriek <karl.schriek@bma.network>
2025-08-24 21:07:54 +0200

    remove nic

--- main.tf
+++ main.tf
@@ -7,17 +7,3 @@ resource "azurerm_public_ip" "public_gateway" {
   sku                 = "Standard"
 }
 
-
-resource "azurerm_network_interface" "private_gateway" {
-  name                = "istio-private-gateway"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = var.ingress_subnet_id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = var.private_gateway_reserved_ip
-  }
-}
-
