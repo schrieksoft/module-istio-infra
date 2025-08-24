59d78a2b6afee3e689a4494d50f38843e6e74e7b
Karl Schriek <karl.schriek@bma.network>
2025-08-24 14:42:44 +0200

    init

---
+++ main.tf
@@ -0,0 +1,22 @@
+
+resource "azurerm_public_ip" "public_gateway" {
+  name                = "istio-public-gateway"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+
+resource "azurerm_network_interface" "private_gateway" {
+  name                = "istio-private-gateway"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.ingress_subnet_id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = var.private_gateway_reserved_ip
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "resource_group_name" {}
+variable "location" {}
+variable "ingress_subnet_id" {}
+variable "private_gateway_reserved_ip" {}
